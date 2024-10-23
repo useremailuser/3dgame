7a76d19528f46a1f637daede6f5a5fa3edc66b44
useremailuser <raphwatson22@gmail.com>
2024-10-24 03:12:31 +1100

    2

--- Assets/FieldOfview.cs
+++ Assets/FieldOfview.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FieldOfview : MonoBehaviour
 {
@@ -7,9 +8,53 @@ public class FieldOfview : MonoBehaviour
     [Range(0,360)]
     public float viewAngle;
 
+    public LayerMask targets;
+    public LayerMask walls;
+
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void Start()
+    {
+        StartCoroutine ("FindTargetsWithDelay", .2f);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds (delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, walls);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position,dirToTarget,dstToTarget,walls))
+                {
+                    visibleTargets.Add(target);
+                }
+
+            }
+        }
+    }
     public Vector3 DirFromAngle(float angleIndegrees, bool angleIsGlobal)
     {
-       
+       if (!angleIsGlobal)
+        {
+            angleIndegrees += transform.eulerAngles.y;
+        }
         return new Vector3(Mathf.Sin(angleIndegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleIndegrees * Mathf.Deg2Rad));
     }
 }
