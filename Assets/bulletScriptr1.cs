82d39fba4ce6e909d8477ee00ab0dd8ac099baed
bigman900 <raphael.watson@studytafensw.edu.au>
2024-11-29 13:02:30 +1100

    +new scripts

---
+++ Assets/bulletScriptr1.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class bulletScriptr : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+           Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.layer == 8)
+        {
+          
+        }
+        
+
+    }
+}
